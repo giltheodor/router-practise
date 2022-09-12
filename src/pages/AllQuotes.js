9a0943083c89f88d8322683ae7de86eace29e886
Gylos <giltheod@gmail.com>
2022-09-12 23:24:04 +0300

    Prompt fixed

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_DATA = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maxim", text: "Learning React is great!" },
+];
+
+const AllQuotes = () => {
+  return <QuoteList quotes={DUMMY_DATA} />;
+};
+
+export default AllQuotes;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import { useParams, Route } from "react-router-dom";
+
+import HighLightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
+
+const DUMMY_DATA = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maxim", text: "Learning React is great!" },
+];
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
+  return (
+    <Fragment>
+      <HighLightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
